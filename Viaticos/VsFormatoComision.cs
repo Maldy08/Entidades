9b7b2ce7b04fc9015b7a4e73f179490231c53084
CARLOS MALDONADO <karloz_0986@hotmail.com>
2023-01-26 11:09:03 -0800

    VsFormatoComision

---
+++ Viaticos/VsFormatoComision.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Viaticos
+{
+    public class VsFormatoComision
+    {
+        public int Oficina { get; set; }
+        public int Ejercicio { get; set; }
+        public int NoViat { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public int NoEmp { get; set; }
+        public int OrigenId { get; set; }
+        public int DestinoId { get; set; }
+        public string Motivo { get; set; } = string.Empty;
+        public DateTime FechaSal { get; set; } = DateTime.Now;
+        public DateTime FechaReg { get; set; } = DateTime.Now;
+        public int Dias { get; set; }
+        public string InforAct { get; set; } = string.Empty;
+        public double Importe { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Materno { get; set; } = string.Empty;
+        public string Paterno { get; set; } = string.Empty;
+        public string DescripcionPuesto { get; set; } = string.Empty;
+        public string CdOrigen { get; set; } = string.Empty;
+        public string CdDestino { get; set; } = string.Empty;
+        public string QuienLoComisiona { get; set; } = string.Empty;
+        public string PuestoQuienLoComisiona { get; set; } = string.Empty;
+        public string EdoOrigen { get; set; } = string.Empty;
+        public string EdoDestino { get; set; } = string.Empty;
+        public string DeptoDescripcion { get; set; } = string.Empty;
+
+    }
+}
